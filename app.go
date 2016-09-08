116341a24ecdefc4a5f0146814aa4daced383470
ethan.thacker <ethan.thacker@kibocommerce.com>
2016-09-08 14:38:55 -0500

    html templates and routing

--- app.go
+++ app.go
@@ -2,17 +2,57 @@ package hello
 
 import (
 	"net/http"
+	"html/template"
+	"path"
+	"log"
+	"os"
 )
 
+//var templates = template.Must(template.ParseGlob("templates/*"))
+
+var tmplt = make(map[string]*template.Template)
+
 func init() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/", templateHandler)
+	tmplt["index"] = template.Must(template.ParseFiles("templates/index.html","templates/layout.html"))
+	tmplt["/about"] = template.Must(template.ParseFiles("templates/about.html","templates/layout.html"))
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w,r,"./views/")
-}
+func templateHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print(r.URL.Path)
+	if r.URL.Path != "/"{
+		fp := path.Join("templates", r.URL.Path + ".html")
 
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w,r,"./views/about.html")
+		info, err := os.Stat(fp)
+		if err != nil {
+			if os.IsNotExist(err) {
+				http.NotFound(w, r)
+				return
+			}
+		}
+
+		if info.IsDir() {
+			http.NotFound(w, r)
+			return
+		}
+
+		if err != nil {
+			// Log the detailed error
+			log.Println(err.Error())
+			// Return a generic "Internal Server Error" message
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+
+		if err := tmplt[r.URL.Path].ExecuteTemplate(w, "layout", nil); err != nil {
+			log.Println(err.Error())
+			http.Error(w, http.StatusText(500), 500)
+		}
+
+
+	} else {
+
+		tmplt["index"].ExecuteTemplate(w,"layout", nil)
+	}
+
 }

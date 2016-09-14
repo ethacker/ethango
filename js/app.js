3ad4d2bdaafceb3d95bad584a1d42fece19d2d17
ethan.thacker <ethan.thacker@kibocommerce.com>
2016-09-14 12:03:00 -0500

    added cookies

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-/**
- * Created by ethan.thacker on 9/9/16.
- */
+
+var Ethan = Ethan || {};
+
+var welcomeback;
+
+Ethan.app = {
+    initPage: function () {
+        if(Cookies.get("returning")=="true"){
+            welcomeback = document.createElement("h2");
+            welcomeback.setAttribute("id","welcome-back-banner");
+            welcomeback.innerHTML = "Welcome Back to EthanThacker.com!";
+            document.getElementById("welcome-back").appendChild(welcomeback);
+        }
+        else {
+            Cookies.set("returning","true",{ expires: 6000 });
+        }
+    }
+
+};

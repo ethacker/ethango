73513d63c2e74b0c8ae14a5f54be38f35e61b5e5
ethan.thacker <ethan.thacker@kibocommerce.com>
2016-09-14 14:01:08 -0500

    added canonical tag and moved welcomeback

--- js/app.js
+++ js/app.js
@@ -6,13 +6,13 @@ var welcomeback;
 Ethan.app = {
     initPage: function () {
         if(Cookies.get("returning")=="true"){
-            welcomeback = document.createElement("h2");
+            welcomeback = document.createElement("p");
             welcomeback.setAttribute("id","welcome-back-banner");
             welcomeback.innerHTML = "Welcome Back to EthanThacker.com!";
             document.getElementById("welcome-back").appendChild(welcomeback);
         }
         else {
-            Cookies.set("returning","true",{ expires: 6000 });
+            Cookies.set("returning","true",{ expires: Infinity });
         }
     }
 

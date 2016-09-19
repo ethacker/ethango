30fffad2e1ec1ac4cf9ed713bb7c2461b50bff08
ethan.thacker <ethan.thacker@kibocommerce.com>
2016-09-19 11:32:10 -0500

    crimes mang

--- static/js/dpd.js
+++ static/js/dpd.js
@@ -3,12 +3,25 @@ Ethan = Ethan || {};
 
 Ethan.dpd = {
     initPage:function(){
+        $("#crimes-table").hide();
         $.get({
             url:"http://localhost:8080/dpdinfo/crimes",
             success:function (data) {
                 data.forEach(function(crime,index){
-                    console.log(crime["beat"]);
-                })
+                    var table = document.getElementById("crimes-table-body");
+                    var row = table.insertRow(index);
+                    row.insertCell(0).innerHTML = crime["location"];
+                    row.insertCell(1).innerHTML = crime["nature_of_call"];
+                    row.insertCell(2).innerHTML = crime["priority"];
+                    row.insertCell(3).innerHTML = crime["date_time"].substr(11,8);
+                    row.insertCell(4).innerHTML = crime["division"];
+                    row.insertCell(5).innerHTML = crime["status"];
+                    row.insertCell(6).innerHTML = crime["unit_number"];
+                });
+                $("#crimes-table").css("display","block");
+            },
+            error:function () {
+
             }
         });
     }
